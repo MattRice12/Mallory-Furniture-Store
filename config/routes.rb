089ec789c407ab5626cf76c222c509381da62b6d
Matt Rice <mattrice12@outlook.com>
2016-07-11 14:30:03 -0500

    Added controller and routes for index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'products/:id', to: 'posts#show'
+  get 'products/', to: 'posts#index'
+  root 'products#index'
+
 end
