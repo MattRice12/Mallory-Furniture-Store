020632bff360ff7cd3ddf8486c7587471f34348f
Matt Rice <mattrice12@outlook.com>
2016-07-11 14:42:15 -0500

    Added html

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,19 @@
 class ProductsController < ApplicationController
 
   def index
-    render template: 'products/index.html.erb' locals: {
+    render template: 'products/index.html.erb', locals: {
       products: Product.all
     }
   end
 
-  def show
-    if Product.exists?(params[:id])
-      render template: 'products/show.html.erb', locals: {
-        product: Product.find(params[:id])
-      }
-    else
-      render html: "Not Found", status: 404
-    end
-  end
+  # def show
+  #   if Product.exists?(params[:id])
+  #     render template: 'products/show.html.erb', locals: {
+  #       product: Product.find(params[:id])
+  #     }
+  #   else
+  #     render html: "Not Found", status: 404
+  #   end
+  # end
 
 end
